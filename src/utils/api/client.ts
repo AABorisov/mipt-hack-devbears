8303f0aac459bf3fd8489a861fea254dcb4d6dca
Andrey Borisov <Andrey.Borisov1@x5.ru>
2020-04-26 10:17:14 +0300

    table

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -1,3 +1,15 @@
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  Authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+};
+
+const options = {
+  credentials: 'include',
+  mode: 'cors',
+  headers,
+};
+
 const get = async (url: string): Promise<any> => {
   return fetch(url).then(response => {
     return response.json();
--- src/utils/api/constant.ts
+++ src/utils/api/constant.ts
@@ -1,6 +1,30 @@
+import { Method } from 'axios';
+
 const API = {
-  transportTypes: '/api/transportTypes',
-  path: '/api/path',
+  plantsData: '/api/plantsData',
+  resourceGroupAtDate: '/api/resourceGroupAtDate',
+  orderById: '/api/orderById',
 };
 
 export default API;
+
+export type BackendEndpointNames = 'plantsData' | 'resourceGroupAtDate' | 'orderById';
+
+export interface EndPoint {
+  method: Method;
+  url: string;
+}
+
+export type BackendEndpoints = {
+  [key in BackendEndpointNames]: EndPoint;
+};
+
+export type BackendEndpointsFunctions = {
+  [key in BackendEndpointNames]: Function;
+};
+
+export const BACKEND_ENDPOINTS: BackendEndpoints = {
+  plantsData: { method: 'get', url: 'equipment-set-types/list' },
+  resourceGroupAtDate: { method: 'get', url: 'equipment-types/list' },
+  orderById: { method: 'get', url: 'streets/list' },
+};
